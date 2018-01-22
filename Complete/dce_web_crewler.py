46023ec8ac2fbebe66744b3a30136d8805cef73b
Philip616 <hyps4413@gmail.com>
2018-01-22 14:48:48 +0800

    2018/1/22

--- Complete/dce_web_crewler.py
+++ Complete/dce_web_crewler.py
@@ -10,9 +10,11 @@ import time
 import pandas as pd
 import random
 import datetime
+from bs4 import BeautifulSoup
 
 
 url_excel = 'http://www.dce.com.cn/publicweb/quotesdata/exportMemberDealPosiQuotesData.html'
+url_click = 'http://www.dce.com.cn/publicweb/quotesdata/memberDealPosiQuotes.html'
 
 #IP池设定
 session = requests.Session()
@@ -21,17 +23,40 @@ session.mount('https://', adapter)
 session.mount('http://', adapter)
 
 prox = {
-        'http':'61.153.67.110:9999',
-        'http':'121.248.112.20:3128'}
+        'https':'116.199.2.196:82',
+        'https':'116.199.2.210:80'}
           
 contract_id=['pp','jm','jd','fb','cs','bb','y','v','p','m','l','j','i','c','a','b']
 
 for i in range(0,len(contract_id)):
     now_date = datetime.datetime(2018,1,9)
     print(contract_id[i])
+    
     while now_date < datetime.datetime.now():
+        
+        tmp = now_date
+        
+        
         split_date = now_date.strftime('%Y-%m-%d').split('-')
         
+# =============================================================================
+#         formData = {'memberDealPosiQuotes.variety':contract_id[i],
+#             'memberDealPosiQuotes.trade_type':'0',
+#             'year':split_date[0],
+#             'month':str(int(split_date[1])-1),
+#             'day':str(int(split_date[2])),
+#             'contract.variety_id':contract_id[i]}
+#         
+#         res = requests.post(url_click,data=formData,stream=True,proxies = prox)
+#         soup = BeautifulSoup(res.text,'lxml')
+#         click = (soup.findAll('li',{'class':'keyWord_65'}))
+#         res.close()
+#         
+#         for con in click:
+#           contract_number = con.get_text(strip=True)
+# =============================================================================
+    
+  
         #网站抓取设定
         formData = {'memberDealPosiQuotes.variety':contract_id[i],
                     'memberDealPosiQuotes.trade_type':'0',
@@ -41,50 +66,26 @@ for i in range(0,len(contract_id)):
                     'contract.contract_id':'all',
                     'contract.variety_id':contract_id[i],
                     'exportFlag':'excel'}
-        tmp = now_date
         
-        res = requests.post(url_excel,data=formData,stream=True,proxies = prox)
-            
+        res = requests.post(url_excel,data=formData,stream=True)
+        res.encoding = 'utf-8-sig'
+
         while(res.status_code != 200):
-            res = requests.post(url_excel,data=formData,stream=True,proxies = prox)
-            time.sleep(random.randrange(2,5))
+            res = requests.post(url_excel,data=formData,stream=True, proxies = prox)
+            time.sleep(random.randrange(2,7))
+
     
-        #存成excel檔
+        #存成excel檔 
         with open('../../大商所更新/'+split_date[0]+split_date[1]+split_date[2]+'.xls', 'wb') as file:
             file.write(res.content)
         res.close()
-        time.sleep(random.randrange(2,5))
+        time.sleep(random.randrange(2,7))
         
         try:
             data = pd.read_excel('../../大商所更新/'+split_date[0]+split_date[1]+split_date[2]+'.xls',encoding='gbk') 
-            data.to_csv('../../各交易所期貨持倉量/大商所/'+contract_id[i]+"/"+split_date[0]+split_date[1]+split_date[2]+'.csv',encoding='gbk',index=None)                 
-            now_date = now_date + datetime.timedelta(days = 1)
+            data.to_csv('../../各交易所期貨持倉量/大商所/'+contract_id[i]+'/'+split_date[0]+split_date[1]+split_date[2]+'.csv',encoding='gbk',index=None)                 
+#            data.to_csv('../../各交易所期貨持倉量/大商所/i_test/'+split_date[0]+split_date[1]+split_date[2] + "_" + contract_number+'.csv',encoding='gbk',index=None)                 
         except Exception as e:
-            #日期加1
-            now_date = now_date + datetime.timedelta(days = 1)
-
-        
-        
+            print(e)
     
-'''    
-#以下是分期獲取的代碼
-for x in range(890,len(date.dropna())):
-    split_date = date['Date'][x].strftime('%Y-%m-%d').split('-')
-    
-    formData = {'memberDealPosiQuotes.variety':'i',
-            'memberDealPosiQuotes.trade_type':'0',
-            'year':split_date[0],
-            'month':str(int(split_date[1])-1),
-            'day':str(int(split_date[2])),
-            'contract.variety_id':'i'} 
-    res = requests.post(url,data=formData,stream=True)
-    soup = BeautifulSoup(res.text,'lxml')
-    click = (soup.findAll('li',{'class':'keyWord_65'}))
-    res.close()
-    
-    tmp = x
-    
-    for con in click: 
-        data = pd.read_excel('./铁矿石_分类/'+split_date[0]+split_date[1]+split_date[2]+'_'+con.get_text(strip=True)+'.xls')                  
-        data.to_csv('./铁矿石_分类2/'+split_date[0]+split_date[1]+split_date[2]+'_'+con.get_text(strip=True)+'.csv',encoding='UTF-8-SIG',index=None)
-'''
+        now_date = now_date + datetime.timedelta(days = 1)
