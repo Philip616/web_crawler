2fdf2b7f4efc3164bf017fd8b335abc7080d4f92
Philip616 <hyps4413@gmail.com>
2018-05-10 08:47:37 +0800

    2018-5-10

--- Complete/dce_web_crewler_option.py
+++ Complete/dce_web_crewler_option.py
@@ -23,14 +23,13 @@ session.mount('https://', adapter)
 session.mount('http://', adapter)
 
 prox = {
-        'https':'116.199.2.196:82',
-        'https':'116.199.2.210:80'}
+        'https':'66.70.147.197:3128',
+        'https':'187.109.81.238:20183'}
           
 contract_id=['m']
-#contract_id=['v']
 
 for i in range(0,len(contract_id)):
-    now_date = datetime.datetime(2018,4,23)
+    now_date = datetime.datetime(2018,4,2)
     print(contract_id[i])
     
     while now_date < datetime.datetime.now():
@@ -40,50 +39,52 @@ for i in range(0,len(contract_id)):
         
         split_date = now_date.strftime('%Y-%m-%d').split('-')
         
-        formData = {'memberDealPosiQuotes.variety':contract_id[i],
-            'memberDealPosiQuotes.trade_type':'1',
-            'year':split_date[0],
-            'month':str(int(split_date[1])-1),
-            'day':str(int(split_date[2])),
-            'contract.variety_id':contract_id[i]}
-        
-        res = requests.post(url_click,data=formData,stream=True,proxies = prox)
-        soup = BeautifulSoup(res.text,'lxml')
-        click = (soup.findAll('li',{'class':'keyWord_65'}))
-        res.close()
-        
-        for con in click:
-            contract_number = con.get_text(strip=True)
+# =============================================================================
+#         formData = {'memberDealPosiQuotes.variety':contract_id[i],
+#             'memberDealPosiQuotes.trade_type':'1',
+#             'year':split_date[0],
+#             'month':str(int(split_date[1])-1),
+#             'day':str(int(split_date[2])),
+#             'contract.variety_id':contract_id[i]}
+#         
+#         res = requests.post(url_click,data=formData,stream=True,proxies = prox)
+#         soup = BeautifulSoup(res.text,'lxml')
+#         click = (soup.findAll('li',{'class':'keyWord_65'}))
+#         res.close()
+#         
+# =============================================================================
+#        for con in click:
+#            contract_number = con.get_text(strip=True)
     
   
             #网站抓取设定
-            formData = {'memberDealPosiQuotes.variety':contract_id[i],
-                        'memberDealPosiQuotes.trade_type':'1',
-                        'year':split_date[0],
-                        'month':str(int(split_date[1])-1),
-                        'day':str(int(split_date[2])),
-                        'contract.contract_id':contract_number,
-                        'contract.variety_id':contract_id[i],
-                        'exportFlag':'excel'}
+        formData = {'memberDealPosiQuotes.variety':contract_id[i],
+                    'memberDealPosiQuotes.trade_type':'1',
+                    'year':split_date[0],
+                    'month':str(int(split_date[1])-1),
+                    'day':str(int(split_date[2])),
+                    'contract.contract_id':'all',
+                    'contract.variety_id':contract_id[i],
+                    'exportFlag':'excel'}
             
-            res = requests.post(url_excel,data=formData,stream=True)
-            res.encoding = 'utf-8-sig'
+        res = requests.post(url_excel,data=formData,stream=True)
+        res.encoding = 'utf-8-sig'
 
-            while(res.status_code != 200):
-                res = requests.post(url_excel,data=formData,stream=True, proxies = prox)
-                time.sleep(random.randrange(2,7))
-    
-        
-            #存成excel檔 
-            with open('../../大商所更新/'+split_date[0]+split_date[1]+split_date[2]+'.xls', 'wb') as file:
-                file.write(res.content)
-            res.close()
+        while(res.status_code != 200):
+            res = requests.post(url_excel,data=formData,stream=True, proxies = prox)
             time.sleep(random.randrange(2,7))
-            
-            try:
-                data = pd.read_excel('../../大商所更新/'+split_date[0]+split_date[1]+split_date[2]+'.xls',encoding='gbk')
-                data.to_csv('../../大商所更新1/'+split_date[0]+split_date[1]+split_date[2]+'_'+contract_number+'.csv',encoding='gbk',index=None)          
-            except Exception as e:
-                print(e)
+
     
+        #存成excel檔 
+        with open('../../大商所更新/'+split_date[0]+split_date[1]+split_date[2]+'.xls', 'wb') as file:
+            file.write(res.content)
+        res.close()
+        time.sleep(random.randrange(2,7))
+        
+        try:
+            data = pd.read_excel('../../大商所更新/'+split_date[0]+split_date[1]+split_date[2]+'.xls',encoding='gbk')
+            data.to_csv('../../各交易所期貨持倉量/大商所/m_option/'+split_date[0]+split_date[1]+split_date[2]+'_all'+'.csv',encoding='gbk',index=None)          
+        except Exception as e:
+            print(e)
+
         now_date = now_date + datetime.timedelta(days = 1)
